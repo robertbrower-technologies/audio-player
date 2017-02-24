d624caf64fba04407e0fd8843f0d3aec65e0ab89
Robert Brower <robertbrower.technologies@gmail.com>
2017-02-24 02:52:27 +0100

    modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,17 @@ export class AppComponent implements OnInit {
   @ViewChild('f') _f;
   playList: any[];
   currentTrack: number;
-
-  subscription: any;
+  subscription1: Subscription;
+  
+  value: number;
+  minValue: number = 0;
+  maxValue: number = 1;
+  minFreq: number = 440;
+  maxFreq: number = 1000;
+  duration: number;
+  context: AudioContext;
+  osc: OscillatorNode;
+  playing: boolean;
 
   constructor(private statusService: StatusService) {
     this.statusService.statuses = [
@@ -30,14 +39,29 @@ export class AppComponent implements OnInit {
       new Status(),
       new Status()
     ];
+
+    
+    this.playing = true;
+    this.context = new (window.AudioContext)();
+    this.osc = this.context.createOscillator(); // instantiate an oscillator
+    this.osc.type = 'sine'; // this is the default - also square, sawtooth, triangle
+    //this.osc.frequency.value = 1000 * this.value; // Hz
+    this.osc.start(); // start the oscillator
   }
 
   ngOnInit() {
     this.testAudio.nativeElement.play();
-    let timer = TimerObservable.create(0, 3000);
-    this.subscription = timer.subscribe(t => {
+    let timer1 = TimerObservable.create(0, 3000);
+    this.subscription1 = timer1.subscribe(t => {
       this.createPlayList();
     });
+
+    this.setValue(.5);
+    this.playTone();
+    //let timer2 = TimerObservable.create(0, 3000);
+    //this.subscription2 = timer2.subscribe(t => {
+    //  this.playTone();
+    //});
   }
 
   public setStatus(index: number, message: string, cssClass: string, audio: any) {
@@ -67,4 +91,45 @@ export class AppComponent implements OnInit {
       this.play(this.playList[this.currentTrack]);
     }
   }
+  
+  // http://stackoverflow.com/questions/6343450/generating-sound-on-the-fly-with-javascript-html5
+  // http://stackoverflow.com/questions/32161832/web-audio-api-oscillator-node-error-cannot-call-start-more-than-once
+  playTone() {
+    if (this.playing) {
+      this.osc.connect(this.context.destination); // connect it to the destination
+      // var that = this;
+      // setTimeout(function() {
+      //   that.stopTone();
+      // }, 1000);
+      setTimeout(() => { this.stopTone(); }, this.duration)
+    }
+  }
+
+  stopTone() {
+    this.osc.disconnect(this.context.destination);
+    if (this.playing) {
+      setTimeout(() => { this.playTone(); }, this.duration)
+    }
+  }
+
+  setValue(value: number) {
+    //this.osc.frequency.value = 1000 * this.value;
+    //scale = (range2.max - range2.min) / (range1.max - range1.min)
+    //new_value = (value - range1.min) * scale + range2.min
+    debugger;
+    this.value = value;
+    var scale = (this.maxFreq - this.minFreq) / (this.maxValue - this.minValue);
+    this.osc.frequency.value = (this.value - this.minValue) * scale + this.minFreq;
+    this.duration = 1050 - this.value * 1000;
+  }
+
+  togglePlaying() {
+    this.playing = !this.playing;
+    if (this.playing) {
+      this.playTone();
+    } else {
+      this.stopTone();
+    }
+  }
+  
 }
